9f560a6b78839281e3b3aff71c5de941421f733b
Nina W <ny.wang@mail.utoronto.ca>
2024-04-10 00:53:59 -0400

    Change amethyst to hardcode numbers instead of mean/std. Change starfruit to use trained multivariable linear regression weights

--- trader.py
+++ trader.py
@@ -95,7 +95,7 @@ logger = Logger()
 
 class Trader:
     POSITION_LIMITS = {"AMETHYSTS": 20, "STARFRUIT": 20}
-    MAX_HISTORY_LENGTH = 30000
+    MAX_HISTORY_LENGTH = {"AMETHYSTS": 0, "STARFRUIT": 20}
     TIMESTAMP_INTERVAL = 100
 
     def sma(self, price_history, history_length, curr_timestamp, pad_beginning=False, initial_avg=0):
@@ -173,18 +173,17 @@ class Trader:
 
         return m, b  # slope, intercept
 
-    def amethyst_algo(self, state, order_depth, price_history):
+    def predict_from_coefs(self, price_history, coef, intercept):
+        pred = intercept
+        for i in range(min(len(price_history), len(coef))):
+            pred += price_history[-i - 1]["price"] * coef[-i - 1]
+        return pred
+
+    def amethyst_algo(self, state, order_depth):
         orders: List[Order] = []
 
-        if "AMETHYSTS" not in price_history or len(price_history["AMETHYSTS"]) == 0:
-            return orders
-
-        mean = self.sma(price_history["AMETHYSTS"], 10000, state.timestamp, True, 10000)
-        std = self.volatility(price_history["AMETHYSTS"], 8000, state.timestamp, mean, True, 1.2)
-        logger.print(f"Amethyst mean is {mean} | std is {std}")
-
-        buy_price = mean - std
-        sell_price = mean + std
+        buy_price = 9999
+        sell_price = 10001
 
         curr_pos = state.position["AMETHYSTS"] if "AMETHYSTS" in state.position else 0
         ask_limit = self.POSITION_LIMITS["AMETHYSTS"] - curr_pos
@@ -194,24 +193,24 @@ class Trader:
             for ask, amt in list(order_depth.sell_orders.items()):
                 ask_amt = abs(amt)
 
-                if int(ask) <= buy_price and ask_limit > 0:
+                if ask_limit > 0 and int(ask) <= buy_price:
                     logger.print(f"AMETHYST BUY {str(min(ask_amt, ask_limit))}x, {ask}")
                     orders.append(Order("AMETHYSTS", ask, min(ask_amt, ask_limit)))
                     ask_limit -= min(ask_amt, ask_limit)
                 elif ask_limit > 0:
-                    orders.append(Order("AMETHYSTS", math.floor(buy_price - std * 1.2), ask_limit))
+                    orders.append(Order("AMETHYSTS", math.floor(buy_price - 2), ask_limit))
                     break
 
         if len(order_depth.buy_orders) != 0:
             for bid, amt in list(order_depth.buy_orders.items()):
                 bid_amt = abs(amt)
 
-                if int(bid) >= sell_price and bid_limit > 0:
+                if bid_limit > 0 and int(bid) >= sell_price:
                     logger.print(f"AMETHYST SELL {str(min(bid_amt, bid_limit))}x, {bid}")
                     orders.append(Order("AMETHYSTS", bid, -min(bid_amt, bid_limit)))
                     bid_limit -= min(bid_amt, bid_limit)
                 elif bid_limit > 0:
-                    orders.append(Order("AMETHYSTS", math.ceil(sell_price + std * 1.2), -bid_limit))
+                    orders.append(Order("AMETHYSTS", math.ceil(sell_price + 2), -bid_limit))
                     break
 
         return orders
@@ -219,46 +218,49 @@ class Trader:
     def starfruit_algo(self, state, order_depth, all_trade_history):
         orders: List[Order] = []
 
-        if "STARFRUIT" not in all_trade_history or len(all_trade_history["STARFRUIT"]) == 0:
+        # Values to tune
+        coef = [0.192134, 0.195654, 0.262699, 0.346080]
+        intercept = 17.363839324130822
+
+        if "STARFRUIT" not in all_trade_history or len(all_trade_history["STARFRUIT"]) < len(coef):
             return orders
 
-        mean = self.sma(all_trade_history["STARFRUIT"], 6000, state.timestamp, False, 5001)
-        std = self.volatility(all_trade_history["STARFRUIT"], 6000, state.timestamp, mean, True, 3.5)
-        logger.print(f"Starfruit mean is {mean} | std is {std}")
-
-        m, b = self.lin_regression(all_trade_history["STARFRUIT"], 8000, state.timestamp)
-        logger.print(f"Starfruit slope is {m}")
-
-        predicted_price = m * (state.timestamp + self.TIMESTAMP_INTERVAL) + b
+        predicted_price = int(round(self.predict_from_coefs(all_trade_history["STARFRUIT"], coef, intercept)))
         logger.print(f"Starfruit predicted price is {predicted_price}")
 
         curr_pos = state.position["STARFRUIT"] if "STARFRUIT" in state.position else 0
         ask_limit = self.POSITION_LIMITS["STARFRUIT"] - curr_pos
         bid_limit = self.POSITION_LIMITS["STARFRUIT"] + curr_pos
 
+        # buying logic
         if len(order_depth.sell_orders) != 0:
+            best_ask, _ = list(order_depth.sell_orders.items())[0]
+
             for ask, amt in list(order_depth.sell_orders.items()):
                 ask_amt = abs(amt)
 
-                if ask_limit > 0 and int(ask) < predicted_price - std * 0.2:
+                if ask_limit > 0 and (int(ask) <= predicted_price - 1 or (curr_pos < 0 and int(ask) == predicted_price)):
                     logger.print(f"STARFRUIT BUY {str(min(ask_amt, ask_limit))}x, {ask}")
                     orders.append(Order("STARFRUIT", ask, min(ask_amt, ask_limit)))
                     ask_limit -= min(ask_amt, ask_limit)
-                elif ask_limit > 0:
-                    orders.append(Order("STARFRUIT", math.floor(predicted_price - std), ask_limit))
-                    break
 
+            if ask_limit > 0:
+                orders.append(Order("STARFRUIT", min(predicted_price - 1, best_ask + 1), ask_limit))
+
+        # selling logic
         if len(order_depth.buy_orders) != 0:
+            best_bid, _ = list(order_depth.buy_orders.items())[0]
+
             for bid, amt in list(order_depth.buy_orders.items()):
                 bid_amt = abs(amt)
 
-                if bid_limit > 0 and int(bid) > predicted_price + std * 0.2:
+                if bid_limit > 0 and (int(bid) >= predicted_price + 1 or (curr_pos > 0 and int(bid) == predicted_price)):
                     logger.print(f"STARFRUIT SELL {str(min(bid_amt, bid_limit))}x, {bid}")
                     orders.append(Order("STARFRUIT", bid, -min(bid_amt, bid_limit)))
                     bid_limit -= min(bid_amt, bid_limit)
-                elif bid_limit > 0:
-                    orders.append(Order("STARFRUIT", math.ceil(predicted_price + std), -bid_limit))
-                    break
+
+            if bid_limit > 0:
+                orders.append(Order("STARFRUIT", max(predicted_price + 1, best_bid - 1), -bid_limit))
 
         return orders
 
@@ -290,15 +292,9 @@ class Trader:
                     "price": mid_price,
                 })
 
-            # remove oldest price history
-            if len(price_history[product]) > 0:
-                earliest_trade = price_history[product][0]
-                while earliest_trade["timestamp"] < state.timestamp - self.MAX_HISTORY_LENGTH:
-                    price_history[product].pop(0)
-
-                    if len(price_history[product]) == 0:
-                        break
-                    earliest_trade = price_history[product][0]
+            # remove the oldest price history
+            while len(price_history[product]) > self.MAX_HISTORY_LENGTH[product]:
+                price_history[product].pop(0)
 
         # calculate orders for each product
         orders = {}
@@ -307,7 +303,7 @@ class Trader:
             res: List[Order] = []
 
             if product == "AMETHYSTS":
-                res = self.amethyst_algo(state, order_depth, price_history)
+                res = self.amethyst_algo(state, order_depth)
             elif product == "STARFRUIT":
                 res = self.starfruit_algo(state, order_depth, price_history)
 
